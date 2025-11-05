e8a8a3b57dd2d672ac36ddf188c75adf7658fb59
lfcg <lfcg@g1c31.cin.ufpe.br>
2025-11-05 16:41:48 -0300

    DELETE ao MIN

--- 04_querys.sql
+++ 04_querys.sql
@@ -77,3 +77,146 @@ UPDATE Animal SET altura = 0.24 WHERE id_animal = 8;
 UPDATE Animal SET altura = 0.25 WHERE id_animal = 9;  
 UPDATE Animal SET altura = 0.65 WHERE id_animal = 10; 
 UPDATE Animal SET altura = 0.22 WHERE id_animal = 11;
+
+--DELETE
+
+DELETE FROM Telefones
+WHERE CPF_pessoa = '11111111101' 
+  AND Numero_telefone = '8133331101';
+
+DELETE FROM Telefones
+WHERE CPF_pessoa = '11111111105' 
+  AND Numero_telefone = '8133331105';
+
+DELETE FROM Telefones
+WHERE CPF_pessoa = '11111111110' 
+  AND Numero_telefone = '8133331110';
+
+--SELECT-FROM-WHERE
+
+SELECT * FROM PESSOA
+WHERE CPF = '11111111111'
+
+SELECT * FROM ENDERECO
+WHERE codigo_endereco = 11
+
+SELECT * FROM Telefones
+WHERE CPF_pessoa = '11111111111'
+
+SELECT * FROM Tutor
+WHERE CPF_tutor = '11111111111'
+
+SELECT * FROM ANIMAL
+WHERE id_animal = 11
+
+
+SELECT * FROM Atendimento
+WHERE id_animal = 11
+
+--BETWEEN   
+
+SELECT Descricao, Preco
+FROM Servico
+WHERE Preco BETWEEN 100.00 AND 300.00;
+
+SELECT nome_animal, data_nascimento
+FROM Animal
+WHERE data_nascimento BETWEEN TO_DATE('2020-01-01', 'YYYY-MM-DD') AND TO_DATE('2021-12-31', 'YYYY-MM-DD');
+
+SELECT Id_atendimento, id_animal, Data_hora_inicio
+FROM Atendimento
+WHERE Data_hora_inicio BETWEEN TO_TIMESTAMP('2025-11-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS') AND TO_TIMESTAMP('2025-11-01 23:59:59', 'YYYY-MM-DD HH24:MI:SS');
+
+--IN
+
+SELECT Id_servico, Descricao, Preco
+FROM Servico
+WHERE Id_servico IN (1, 8, 10);
+
+SELECT nome, email
+FROM Pessoa
+WHERE CPF IN (
+    SELECT CPF_tutor
+    FROM Tutor
+    WHERE status_tutor = 'Ativo');
+
+SELECT nome, email
+FROM Pessoa
+WHERE CPF IN (
+    SELECT CPF_tutor
+    FROM Tutor
+    WHERE status_tutor = 'Inativo');
+
+--LIKE
+
+SELECT nome_animal, especie
+FROM Animal
+WHERE especie LIKE 'Cachorro%';
+
+SELECT Descricao, Preco
+FROM Servico
+WHERE Descricao LIKE '%Castração%';
+
+SELECT nome_animal
+FROM Animal
+WHERE nome_animal LIKE '____';
+
+--IS NULL / IS NOT NULL
+
+SELECT Id_atendimento, Status_agendamento, Data_hora_inicio
+FROM Atendimento
+WHERE Data_hora_fim IS NULL;
+
+SELECT id_pagamento, nr_parcela, valor_parcela, data_pagamento
+FROM Parcela_Pagamento
+WHERE data_pagamento IS NOT NULL;
+
+SELECT Id_atendimento, Status_agendamento, Data_hora_inicio, Data_hora_fim
+FROM Atendimento
+WHERE Data_hora_fim IS NOT NULL;
+
+-- INNER JOIN
+
+SELECT
+    a.nome_animal,
+    a.especie,
+    p.nome AS nome_tutor
+FROM Animal a
+INNER JOIN Tutor t ON a.CPF_tutor = t.CPF_tutor
+INNER JOIN Pessoa p ON t.CPF_tutor = p.CPF;
+
+SELECT
+    att.Id_atendimento,
+    s.Descricao,
+    s.Preco
+FROM Atendimento att
+INNER JOIN Servico s ON att.id_servico = s.Id_servico;
+
+SELECT
+    att.Data_hora_inicio,
+    a.nome_animal,
+    p.nome AS nome_veterinario
+FROM Atendimento att
+INNER JOIN Animal a ON att.id_animal = a.id_animal
+INNER JOIN Veterinario v ON att.CPF_veterinario = v.CPF_veterinario
+INNER JOIN Pessoa p ON v.CPF_veterinario = p.CPF;
+
+-- MAX e MIN
+
+SELECT
+    Descricao,
+    Preco
+FROM Servico
+WHERE Preco = (SELECT MAX(Preco) FROM Servico);
+
+SELECT
+    nome_animal,
+    especie,
+    peso
+FROM Animal
+WHERE peso = (SELECT MIN(peso) FROM Animal);
+
+SELECT
+    MIN(data_nascimento) AS Animal_Mais_Velho,
+    MAX(data_nascimento) AS Animal_Mais_Novo
+FROM Animal;
